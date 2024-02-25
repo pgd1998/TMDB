be4fc77bc24888f30a95776b4aeb088fe89d1a29
Poorvith <poorvithgowda1998@gmail.com>
2024-02-25 12:13:13 +1100

    feat: movie details

--- js/index.js
+++ js/index.js
@@ -16,20 +16,18 @@ function onClick() {
             "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMTE2YTY3OGYxMzZmNzU2YTVlOTI5ZjU2YWRhNzFiNCIsInN1YiI6IjY1ZDgwNTIxOTQ0YTU3MDE2MzIzYTkzNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Nen7Oln3vKJzKnc5LjdWZ4_TzxO-YseDiOwLRASDkC8",
         },
       };
-
-      fetch(url, options)
-        .then((res) => res.json())
-        .then((json) => {
-          if (json.results && json.results.length > 0) {
-            const moviedetails = json.results[0];
-            document.getElementById("movieName").textContent =
-              moviedetails.title;
-            document.getElementById("movieOverview").textContent =
-              moviedetails.overview;
-          } else {
+        fetch(url, options)
+            .then((res) => res.json())
+            .then((json) => {
+                if (json.results && json.results.length > 0) {
+                const moviedetails = json.results[0];
+                    localStorage.setItem('moviedetails', JSON.stringify(moviedetails));
+                    console.log("movie details", localStorage.getItem('moviedetails'));
+                    window.location.href = "movieDetails.html";
+                    } else {
             alert("No movie found");
           }
-        })
+            })
         .catch((err) => console.error("error:" + err));
     }
   });
@@ -53,4 +51,7 @@ function onClose() {
   eraseMovieOverview.textContent = "";
 }
 
-function displayMovie() {}
+function back() {
+  window.location.href = "index.html";
+}
+
